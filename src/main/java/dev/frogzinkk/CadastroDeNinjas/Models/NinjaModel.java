b61cc13ce5be6b7d6bdcc9606a74b202872039f2
frogzinkkk <andre.liborio2005@gmail.com>
2025-06-17 23:46:17 -0300

    configurando  ninja model

---
+++ src/main/java/dev/frogzinkk/CadastroDeNinjas/Models/NinjaModel.java
@@ -0,0 +1,26 @@
+package dev.frogzinkk.CadastroDeNinjas.Models;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "tb_cadastro")
+public class NinjaModel  {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+  private String nome;
+  private String email;
+  private int idade;
+
+
+  public NinjaModel() {
+  }
+
+  public NinjaModel(String nome, String email, int idade) {
+    this.nome = nome;
+    this.email = email;
+    this.idade = idade;
+  }
+
+}
