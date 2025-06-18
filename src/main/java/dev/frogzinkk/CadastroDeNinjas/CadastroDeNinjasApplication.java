58efc91a71481bf562bea1991a8ff0b4a8658f88
frogzinkkk <andre.liborio2005@gmail.com>
2025-06-17 22:39:57 -0300

    criando controller

---
+++ src/main/java/dev/frogzinkk/CadastroDeNinjas/CadastroDeNinjasApplication.java
@@ -0,0 +1,14 @@
+package dev.frogzinkk.CadastroDeNinjas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CadastroDeNinjasApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(CadastroDeNinjasApplication.class, args);
+	}
+
+}
