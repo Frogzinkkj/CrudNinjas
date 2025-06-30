0a2c71712d352c845ac6d6764939823f2bdf479e
frogzinkkk <andre.liborio2005@gmail.com>
2025-06-30 14:05:21 -0300

    build:connect db h2 to spring application

--- src/main/java/dev/frogzinkk/CadastroDeNinjas/Ninjas/NinjaRepository.java
+++ src/main/java/dev/frogzinkk/CadastroDeNinjas/Ninjas/NinjaRepository.java
@@ -3,4 +3,10 @@ package dev.frogzinkk.CadastroDeNinjas.Ninjas;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface NinjaRepository extends JpaRepository<NinjaModel,Long> {
+
+
+
+
+
+
 }
